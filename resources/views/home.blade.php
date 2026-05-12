51a07ed71497cf8cfc41a2e1a42caf2e47fd0ccb
mriyani <ryaneq@gmail.com>
2026-05-12 23:59:53 +0800

    feat: implement ChirpController index method passing data to home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,5 +13,16 @@
                 </div>
             </div>
         </div>
+        @foreach ($chirps as $chirp)
+        <div class="card bg-base-100 shadow mt-8">
+            <div class="card-body">
+                <div>
+                    <div class="font-semibold">{{ $chirp['author'] }}</div>
+                    <div class="mt-1">{{ $chirp['message'] }}</div>
+                    <div class="text-sm text-gray-500 mt-2">{{ $chirp['time'] }}</div>
+                </div>
+            </div>
+        </div>
+        @endforeach
     </div>
 </x-layout>
