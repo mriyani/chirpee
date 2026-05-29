945243d86c78747773c7c43b0ff3c6d03de540bd
mriyani <ryaneq@gmail.com>
2026-05-29 11:27:29 +0800

    feat: implemet edit and delete chirp

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -62,24 +62,35 @@ class ChirpController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Chirp $chirp)
     {
-        //
+        // We'll add authorization in lesson 11
+        return view('chirps.edit', compact('chirp'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Chirp $chirp)
     {
-        //
+        // Validate
+        $validated = $request->validate(
+            [
+                'message' => 'required|string|max:255',
+            ],
+            [
+                'message.required' => 'Please write something to chirp!',
+                'message.max' => 'Chirps must be 255 characters or less.'
+            ]
+        );
+
+        // Update
+        $chirp->update($validated);
+
+        return redirect('/')->with('success', 'Your chirp has been updated!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function destroy(Chirp $chirp)
     {
-        //
+        $chirp->delete($chirp->id);
+
+        return redirect('/')->with('success', 'Your chirp has been deleted!');
     }
 }
