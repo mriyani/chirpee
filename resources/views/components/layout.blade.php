7111ad4b032ba864b016e41510a2c0c2a94eb227
mriyani <ryaneq@gmail.com>
2026-06-11 02:27:32 +0800

    feat(auth): implement Login/Logout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -20,12 +20,12 @@
         <div class="navbar-end gap-2">
             @auth
             <span class="text-sm">{{ auth()->user()->name }}</span>
-            <form method="POST" action="/logout" class="inline">
+            <form method="POST" action="{{ route('logout') }}" class="inline">
                 @csrf
                 <button type="submit" class="btn btn-ghost btn-sm">Logout</button>
             </form>
             @else
-            <a href="/login" class="btn btn-ghost btn-sm">Sign In</a>
+            <a href="{{ route('login') }}" class="btn btn-ghost btn-sm">Sign In</a>
             <a href="{{ route('register') }}" class="btn btn-primary btn-sm">Sign Up</a>
             @endauth
         </div>
